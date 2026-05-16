1014899b19cced0c7e8ee9a70906a472d54986ac
amethyst.qq <125355796+shaypishgithub@users.noreply.github.com>
2026-05-16 17:37:18 +0300

    Create base.lua

---
+++ vertelvsepoel/megahack/loader/base.lua
@@ -0,0 +1,33 @@
+-- MEGAHACK BASE CONFIG
+return {
+    baseUrl = "https://raw.githubusercontent.com/shaypishgithub/infinity/refs/heads/main/vertelvsepoel/megahack/base",
+
+    -- Здесь можно добавлять новые категории
+    categories = {
+        Brookhaven       = "brookhaven.lua",
+        Evade            = "evade.lua",
+        MM2              = "mm2.lua",
+        MegaHack         = "megapizda.lua",
+        Hacks            = "hacks.lua",
+        Admins           = "admin.lua",
+        Animations       = "animation.lua",
+        FE               = "fe.lua",
+        RagdollEngine    = "ragdoll.lua",
+        NaturalDisaster  = "naturaldisaster.lua",
+        BloxFruit        = "bloxfruit.lua",
+        BladeBall        = "bladeball.lua",
+        StealBrainRoot   = "stealbrainroot.lua",
+        TowerOfHell      = "tower.lua",
+        AdoptMe          = "adoptme.lua",
+        GrowGarden       = "growgarden.lua",
+        Night            = "night.lua",
+        Weird            = "weird.lua",
+        DuelsMVS         = "duelsmvs.lua",
+        ViolenceDistrict = "violencedistrict.lua",
+        IKEA3008         = "3008.lua",
+        Rivals           = "rivals.lua",
+        FORSAKEN         = "forsaken.lua",
+        LootUp           = "lootup.lua",
+        -- Добавляй новые сюда ↓
+    }
+}
