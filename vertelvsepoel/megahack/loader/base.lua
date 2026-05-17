f91acc104a42f3f6d09cf0551ca40b76020e92b3
amethyst.qq <125355796+shaypishgithub@users.noreply.github.com>
2026-05-17 21:03:41 +0300

    Update base.lua

--- vertelvsepoel/megahack/loader/base.lua
+++ vertelvsepoel/megahack/loader/base.lua
@@ -1,33 +1,30 @@
-
+-- base.lua
 return {
-    baseUrl = "https://raw.githubusercontent.com/shaypishgithub/infinity/refs/heads/main/vertelvsepoel/megahack/base",
-
-
-    categories = {
-        Brookhaven       = "brookhaven.lua",
-        Evade            = "evade.lua",
-        MM2              = "mm2.lua",
-        MegaHack         = "megapizda.lua",
-        Hacks            = "hacks.lua",
-        Admins           = "admin.lua",
-        Animations       = "animation.lua",
-        FE               = "fe.lua",
-        RagdollEngine    = "ragdoll.lua",
-        NaturalDisaster  = "naturaldisaster.lua",
-        BloxFruit        = "bloxfruit.lua",
-        BladeBall        = "bladeball.lua",
-        StealBrainRoot   = "stealbrainroot.lua",
-        TowerOfHell      = "tower.lua",
-        AdoptMe          = "adoptme.lua",
-        GrowGarden       = "growgarden.lua",
-        Night            = "night.lua",
-        Weird            = "weird.lua",
-        DuelsMVS         = "duelsmvs.lua",
-        ViolenceDistrict = "violencedistrict.lua",
-        IKEA3008         = "3008.lua",
-        Rivals           = "rivals.lua",
-        FORSAKEN         = "forsaken.lua",
-        LootUp           = "lootup.lua",
-   
-    }
+    baseUrl = "https://raw.githubusercontent.com/shaypishgithub/infinity/refs/heads/main/vertelvsepoel/megahack/base",
+    categories = {
+        Brookhaven       = "brookhaven.lua",
+        Evade            = "evade.lua",
+        MM2              = "mm2.lua",
+        MegaHack         = "megapizda.lua",
+        Hacks            = "hacks.lua",
+        Admins           = "admin.lua",
+        Animations       = "animation.lua",
+        FE               = "fe.lua",
+        RagdollEngine    = "ragdoll.lua",
+        NaturalDisaster  = "naturaldisaster.lua",
+        BloxFruit        = "bloxfruit.lua",
+        BladeBall        = "bladeball.lua",
+        StealBrainRoot   = "stealbrainroot.lua",
+        TowerOfHell      = "tower.lua",
+        AdoptMe          = "adoptme.lua",
+        GrowGarden       = "growgarden.lua",
+        Night            = "night.lua",
+        Weird            = "weird.lua",
+        DuelsMVS         = "duelsmvs.lua",
+        ViolenceDistrict = "violencedistrict.lua",
+        IKEA3008         = "3008.lua",
+        Rivals           = "rivals.lua",
+        FORSAKEN         = "forsaken.lua",
+        LootUp           = "lootup.lua",
+    }
 }
